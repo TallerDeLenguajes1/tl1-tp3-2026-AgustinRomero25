1d4e3b902ba5bc0f3acf5f42d5cdeecda9471f96
Agustin Romero <agustin.romero.31.7.00@gmail.com>
2026-04-30 23:31:21 -0300

    modificada la funcion del branch como se pidio en la parte 2

--- ArregloDeNombres.c
+++ ArregloDeNombres.c
@@ -10,18 +10,30 @@ void mostrarPersonas (char* vector [])
     }
 }
 
+int buscarNombre (char* vector [], char clave [])
+{
+    for (int i = 0; i < 5; i++)
+    {
+        if (strstr(vector[i], clave) != NULL)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
 int main ()
 {
-    
     char* nombres [5];
     char buffer [100];
+    char clave [20] = "santos perez";
     printf("Ingrese 5 nombres de personas\n");
 
     for (int i = 0; i < 5 ; i++)
     {
         printf("Ingrese el nombre %d: \n", i + 1);
         fflush(stdin);
-        scanf("%s", buffer);
+        gets (buffer);
         int longitud = strlen (buffer) + 1;
         nombres [i] = (char*) malloc(longitud * sizeof(char)); 
         strcpy(nombres[i], buffer);
@@ -29,6 +41,16 @@ int main ()
 
     mostrarPersonas(nombres);
     printf("\n");
+
+    int resultado = buscarNombre(nombres, clave);
+
+    if(resultado == -1)
+    {
+        printf("Clave incorrecta.\n");
+    } else
+    {
+        printf("Exito. Clave correcta en el puesto %d.\n", resultado + 1);
+    }
 
     for (int i = 0; i < 5; i++)
     {
